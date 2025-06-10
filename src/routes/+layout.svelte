e900da6f9b3d8048232fbc97cb93ac9ef3aa36ca
lmfaole <olejorgenbakken@gmail.com>
2025-06-10 18:51:11 +0200

    lmfaole.party

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { LayoutProps } from './$types';
+
+	let { data, children }: LayoutProps = $props();
+</script>
+
+<main>
+	{@render children()}
+</main>
+
+<footer>
+	<h2>Meta</h2>
+	<dl>
+		<dt>Updated at</dt>
+		<dd>{new Date(data.updated_at).toLocaleDateString("en", { day: "numeric", month: "long", year: "numeric" })}</dd>
+		<dt>Size</dt>
+		<dd>{data.size} KB</dd>
+	</dl>
+</footer>
