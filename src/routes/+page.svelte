296b6a3bff0847cb67bfd6f11894d234c148414f
lmfaole <olejorgenbakken@gmail.com>
2025-06-10 20:38:38 +0200

    content for home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-<h1>Welcome to lmfaole.party</h1>
+<svelte:head>
+	<title>Ole Jørgen Bakken</title>
+</svelte:head>
+
+<h1>Welcome to lmfaole.&shy;party</h1>
+
+<p>Frontend developer at <a href="https://www.fremtind.no" lang="no">Fremtind Forsikring</a>, doing design system, CSS
+	and
+	accessibility stuff.</p>
