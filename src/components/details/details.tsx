7ba61131b055ac3110fdb9d15cdca92d6be3275b
OJ <olejorgenbakken@gmail.com>
2025-08-03 13:35:27 +0200

    flere eksempler på bruk

--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -4,10 +4,10 @@ import type { DetailsTypes } from "./details.types.ts";
 import "./details.css";
 
 export const Details = (props: DetailsTypes) => {
-	const { summary, children, ...rest } = props;
+	const { summary = "Les mer", children, ...rest } = props;
 	return (
 		<details {...rest}>
-			{summary && <summary>{summary}</summary>}
+			<summary>{summary}</summary>
 			<div>{children}</div>
 		</details>
 	);
@@ -16,7 +16,41 @@ export const Details = (props: DetailsTypes) => {
 export const detailsInfo: ComponentInfoTypes = {
 	name: "Details",
 	category: "handlinger",
-	examples: [{ code: <Details summary={"React"}>Detaljer</Details> }],
+	examples: [
+		{ code: <Details>Detaljer</Details> },
+		{
+			title: "Liste",
+			code: (
+				<>
+					<Details summary={"Detaljer 1"}>Detaljer</Details>
+					<Details summary={"Detaljer 2"}>Detaljer</Details>
+					<Details summary={"Detaljer 3"}>Detaljer</Details>
+				</>
+			),
+		},
+		{
+			title: "Liste med trekkspill",
+			description: (
+				<p>
+					Ved å sette samme <code>name</code>-egenskap vil du kunne kontrollere
+					at kun én er åpen om gangen.
+				</p>
+			),
+			code: (
+				<>
+					<Details summary={"Detaljer 1"} name={"detaljer"}>
+						Detaljer
+					</Details>
+					<Details summary={"Detaljer 2"} name={"detaljer"}>
+						Detaljer
+					</Details>
+					<Details summary={"Detaljer 3"} name={"detaljer"}>
+						Detaljer
+					</Details>
+				</>
+			),
+		},
+	],
 	spec: "https://html.spec.whatwg.org/#the-details-element",
 };
 
