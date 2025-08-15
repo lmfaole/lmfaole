e0148398c199c4ed2cc03006e8d967b311f60ae5
OJ <olejorgenbakken@gmail.com>
2025-08-15 22:52:42 +0200

    bedre tekst

--- src/elements/grouping/unordered-list/unordered-list.info.ts
+++ src/elements/grouping/unordered-list/unordered-list.info.ts
@@ -7,6 +7,6 @@ export const unorderedListInfo: ElementInfoType = {
 		"The unordered-list element represents a list of items, where the order of the items is not important — that is, where changing the order would not materially change the meaning of the document.",
 	spec: "https://html.spec.whatwg.org/multipage/grouping-content.html#the-ul-element",
 	category: "gruppering",
-	aliases: ["usortert lists"],
+	aliases: ["usortert liste"],
 	examples: [UnorderedListExample()],
 };
