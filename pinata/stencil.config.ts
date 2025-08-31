a15a2093f0e60f8b2b2baef53383aba246cbb9f0
lmfaole <olejorgenbakken@gmail.com>
2025-08-31 20:50:00 +0200

    global styles

--- pinata/stencil.config.ts
+++ pinata/stencil.config.ts
@@ -2,6 +2,7 @@ import {Config} from '@stencil/core';
 
 export const config: Config = {
   namespace: 'pinata',
+  globalStyle: 'src/styles/global.css',
   outputTargets: [
     {
       type: 'dist',
