71a7256be01a6219d37fe22cf465ca524157c0fb
OJ <olejorgenbakken@gmail.com>
2025-09-25 22:41:16 +0200

    litt dato-greier

---
+++ utilities/date.js
@@ -0,0 +1,22 @@
+function toInputFormat(dateToBeFormatted) {
+    const date = new Date(dateToBeFormatted);
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    if (day < 10) {
+        day = `0${day}`
+    }
+
+    if (month < 10) {
+        month = `0${month}`
+    }
+
+    return `${year}-${month}-${day}`;
+}
+
+const norwgegianFormat = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric"
+}
