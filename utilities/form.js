bf4190b4680bb94da09a02a6502eb4bf37d74a72
OJ <olejorgenbakken@gmail.com>
2025-09-28 17:01:53 +0200

    mer dato-greier

---
+++ utilities/form.js
@@ -0,0 +1,26 @@
+function createDescriptionListFromInputs(outputNode, inputElements) {
+    const output = document.getElementById(outputNode);
+
+    const list = document.createElement("dl");
+    list.ariaLabel = "Dine valg";
+    const listHeading = document.createElement("h2");
+    listHeading.innerText = "Dine valg";
+
+    inputElements.map((input) => {
+        if(input.value) {
+            const descriptionTerm = document.createElement("dt");
+            descriptionTerm.innerText = input.previousElementSibling.innerText;
+
+            const descriptionDetail = document.createElement("dd");
+            if(input.type === "date") {
+                descriptionDetail.innerText = new Date(input.value).toLocaleDateString(navigator.language, {...defaultDateFormat });
+            } else {
+            descriptionDetail.innerText = input.value;
+            }
+
+            list.append(descriptionTerm, descriptionDetail);
+        }
+    });
+
+    output.replaceChildren(listHeading, list);
+}
