b6b1fd37b9e82a9db57575abf8f27854f1921b23
OJ <olejorgenbakken@gmail.com>
2025-10-04 19:21:50 +0200

    mer skjema

--- scripts/utilities/form.js
+++ scripts/utilities/form.js
@@ -3,16 +3,17 @@ const forms = Array.from(document.getElementsByTagName("form"));
 function submitForm(form) {
     const formData = new FormData(form);
     let formInfo = {
-        meta: {
-            name: form.name,
-            submitDate: new Date(),
-        },
+        formName: form.name,
+        submitDate: new Date(),
+        valid: form.checkValidity(),
         answers: []
     };
 
-    console.groupCollapsed(`Skjema: ${formInfo.meta.name} (fullført ${formInfo.meta.submitDate.toLocaleTimeString()})`);
+    console.groupCollapsed(`Skjema: ${formInfo.formName} (fullført ${formInfo.submitDate.toLocaleTimeString()})`);
     console.debug(formInfo);
-    for (const [key, value] of formData) {
+
+    for (let [key, value] of formData) {
+        console.log(value);
         if (value) {
             if (formData.getAll(key).length > 1) {
                 formData.set(key, JSON.stringify(formData.getAll(key)));
@@ -24,10 +25,83 @@ function submitForm(form) {
     }
     console.table(formInfo.answers);
     console.groupEnd();
+
     return formInfo;
 }
 
-forms.map((form) => form.addEventListener("submit", function (ev) {
-    ev.preventDefault();
-    submitForm(form);
-}));
+function handleFormNavigation(form) {
+    const currentSection = form.querySelector("section[aria-current=step]");
+    const nextSection = currentSection.nextElementSibling;
+
+    let currentInputs = Array.from(currentSection.querySelectorAll("input"));
+
+    currentInputs.map((input) => {
+        if (input.type !== "checkbox") {
+            input.required = true
+        }
+    });
+
+    let invalidInputs = currentInputs.filter((input) => !input.validity.valid);
+    let hasInvalidInputs = invalidInputs.length;
+
+    if (nextSection && !hasInvalidInputs) {
+        currentSection.setAttribute("aria-current", "");
+        nextSection.setAttribute("aria-current", "step");
+    } else if (hasInvalidInputs) {
+        form.reportValidity();
+    } else {
+        submitForm(form);
+    }
+}
+
+forms.map((form) => {
+    form.addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        submitForm(form);
+    });
+
+    form.addEventListener("keypress", (ev) => {
+        if (ev.key === "Enter") {
+            ev.preventDefault();
+            handleFormNavigation(form);
+        }
+    });
+
+    // Multi-step forms
+    if (form.classList.contains("steps")) {
+        const sections = form.querySelectorAll("section");
+        if (sections) {
+            Array.from(sections).map((section) => {
+                section.id = section.querySelector("h2").innerText;
+
+                const nextSection = section.nextElementSibling;
+                const previousSection = section.previousElementSibling;
+                const currentSection = section.hasAttribute("aria-current");
+                if (!currentSection) {
+                    sections[0].setAttribute("aria-current", "step");
+                }
+
+                const nextSectionButton = document.createElement("button");
+                nextSectionButton.classList.add("next");
+                nextSectionButton.type = nextSection ? "button" : "submit";
+                nextSectionButton.innerText = nextSection ? "Neste" : "Send";
+                nextSectionButton.addEventListener("click", () => handleFormNavigation(form));
+
+                const previousSectionButton = document.createElement("button");
+                previousSectionButton.classList.add("previous");
+                previousSectionButton.type = previousSection ? "button" : "reset";
+                previousSectionButton.innerText = previousSection ? "Forrige" : "Tilbakestill";
+                previousSectionButton.addEventListener("click", () => {
+                    if (previousSection) {
+                        section.removeAttribute("aria-current");
+                        previousSection.setAttribute("aria-current", "step");
+                    }
+                });
+
+                const footer = document.createElement("footer");
+                footer.append(nextSectionButton, previousSectionButton);
+                section.append(footer);
+            });
+        }
+    }
+});
